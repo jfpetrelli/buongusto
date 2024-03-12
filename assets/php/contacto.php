954d9d1a546fc9c908fe3e34c29cfaca954caacc
ToTTi <jfpetrelli@gmail.com>
2024-03-12 18:32:52 -0300

    .

--- assets/php/contacto.php
+++ assets/php/contacto.php
@@ -9,14 +9,16 @@ require("class.smtp.php");
 // Valores enviados desde el formulario
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
     header('Location: ../../index.html');
+    exit;
 }
 
 if (!isset($_POST["name"]) || !isset($_POST["email"]) || !isset($_POST["message"]) || !isset($_POST["subject"])) {
-    die("Es necesario completar todos los datos del formulario");
+     header('Location: ../../index.html');
+     exit;
 }
 
 $nombre = $_POST["name"];
-$emailA = $_POST["email"];
+$email = $_POST["email"];
 $asunto = $_POST["subject"];
 $mensaje = $_POST["message"];
 
@@ -24,20 +26,23 @@ include("../../config/config.php");
 
 $mail = new PHPMailer(true);
 $mail->isSMTP();
+
+$mail->setFrom($email, " $nombre $apellido");
+$mail->addAddress($emailDestino, "Sitio Web");
+$mail->Subject = "Mensaje desde la web: \n" . $asunto;
+$mail->msgHTML($message);
+
+// $mail->Body = nl2br($mensaje);
+
 $mail->Host = $smtpHost;
 $mail->SMTPAuth = true;
-$mail->Username = $smtpUsuario;
-$mail->Password = $smtpClave;
 $mail->SMTPSecure = 'ssl'; // Utiliza SSL para cifrado
 $mail->Port = 465;
 $mail->CharSet = "utf-8";
 
-$mail->setFrom($emailA, '"' . $nombre . '" <' . $emailA . '>');
-$mail->addAddress($emailDestino);
 
-$mail->isHTML(true);
-$mail->Subject = "Mail desde la web: \n" . $asunto;
-$mail->Body = nl2br($mensaje);
+
+
 
 try {
     $mail->send();
